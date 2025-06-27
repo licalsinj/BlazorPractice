ea230d7cbf4409ef71ec61097aaeefe295094932
licalsinj <licalsinj@gmail.com>
2025-06-27 17:25:54 -0500

    Got the component set up to live edit the decorations of the user's names.

--- BlazorTutorial1/Models/User.cs
+++ BlazorTutorial1/Models/User.cs
@@ -5,17 +5,37 @@ namespace BlazorTutorial1.Models
     [Serializable]
     public class User
     {
-        public Guid id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        public required Guid Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        // TODO Rework this so it's a dictionary of options or something more dynamic
+        //      so you can build the settings table better in UserEdit.razor
+        
         //Boolean tracking if name should be italisized
-        public bool isItal { get; set; }
+        public required bool IsItal { get; set; }
         //Boolean tracking if name should be bolded
-        public bool isBold { get; set; }
+        public required bool IsBold { get; set; }
         //Boolean tracking if name should be underlined
-        public bool isUnln { get; set; }
+        public required bool IsUnln { get; set; }
         //Boolean tracking if name should be striked through
-        public bool isStrk { get; set; } 
+        public required bool IsStrk { get; set; }
+        public string DecoratedName()
+        {
+            string returnName = this.FirstName + " " + this.LastName;
+            if(IsItal){
+                returnName = "<i>" + returnName + "</i>"; 
+            }
+            if(IsBold){
+                returnName = "<b>" + returnName + "</b>"; 
+            }
+            if(IsUnln){
+                returnName = "<u>" + returnName + "</u>"; 
+            }
+            if(IsStrk){
+                returnName = "<del>" + returnName + "</del>"; 
+            }
+            return returnName;
+        }
     }
 }
